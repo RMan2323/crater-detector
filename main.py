a08d6a50885ff0d01f91dd33d29b920bcc1ee37b
RMan2323 <raghuveer23verma@gmail.com>
2025-06-18 19:59:36 +0530

    epochs 48-55 (train19)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO("runs/detect/train17/weights/best.pt")
+model = YOLO("runs/detect/train18/weights/best.pt")
 
-results = model.train(epochs=7, pretrained=True, save_period=1, half=True)
+results = model.train(epochs=8, pretrained=True, save_period=1, half=True)
